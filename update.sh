d01af5549cbaed604a6ca740bbfe9647bd22f7b1
smartegg <lazysmartegg>
2012-05-01 08:48:11 -0700

    update the shell

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sFf $HOME/.linux_config/bin  $HOME/bin
+ln -sf $HOME/.linux_config/.gitignore $HOME/.gitignore_global
+ln -sf $HOME/.linux_config/.bash_profile  $HOME/.bash_profile
+ln -sf $HOME/.linux_config/.gitconfig $HOME/.gitconfig
