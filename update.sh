93e565c02b58def4e9f2190d327f6096f2a73a7e
smartegg <lazysmartegg>
2012-05-04 13:10:58 +0800

    add some files

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@ ln -sFf $HOME/.linux_config/bin  $HOME/bin
 ln -sf $HOME/.linux_config/.gitignore $HOME/.gitignore_global
 ln -sf $HOME/.linux_config/.bash_profile  $HOME/.bash_profile
 ln -sf $HOME/.linux_config/.gitconfig $HOME/.gitconfig
+
+ln -sFf $HOME/.linux_config/astyle  $HOME/.vim/astyle
+ln -sFf $HOME/.linux_config/myultisnips $HOME/.vim/myultisnips
