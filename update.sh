1103c943d902667e5a2aa32b6eed8cc7d23e0e77
smartegg <lazysmartegg>
2012-07-26 16:52:09 +0800

    update

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ ln -sf $HOME/.linux_config/.vimrc.bundles.local   $HOME/.vimrc.bundles.local
 ln -sf $HOME/.linux_config/.vimrc.local    $HOME/.vimrc.local
 
 ln -sFf $HOME/.linux_config/astyle  $HOME/.vim/astyle
-ln -sFf $HOME/.linux_config/myultisnips $HOME/.vim/myultisnips
+ln -sFf $HOME/.linux_config/myultisnips $HOME/.vim/bundle/ultisnips/myultisnips
+ln -sFf $HOME/.linux_config/c-support  $HOME/.vim/c-support
 
 
